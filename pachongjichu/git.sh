505339fb2e10345868144f1819c28f949a563c5f
helloxizhao <2210486930@qq.com>
2017-04-12 15:44:01 +0800

    commit pachongjichu

--- pachongjichu/git.sh
+++ pachongjichu/git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-git remote add git@github.com:helloxizhao/$1.git
-git add ./*
-git commit -m  "commit $1"
-git push origin master
+if [ $# -eq  1 ];then
+    git remote add origin git@github.com:helloxizhao/$1.git
+    git add ./*
+    git commit -m  "commit $1"
+    git push origin master
+else echo "Usage $(basename $0)" git_project_name
+fi
